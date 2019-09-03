94b5bba75280859d3616b032bbdfb8d3af8464fa
Mason Roberts <mrobe18@gmail.com>
2019-09-03 19:22:54 -0400

    mouseover Keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// mouseover - change the bus img on mouse event
+
+const busB = document.querySelector('.intro img');
+busB.addEventListener('mouseover',(e) => {
+    busB.src = 'img/fun-busB.jpg';
+})
+
+//keydown - key down event
+const keyPress = document.addEventListener('keydown',(e) => {
+    alert(`You pressed the ${e.key} key`);
+})
+
+//wheel
